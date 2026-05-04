66c52cd7a9dc297ff1da9b4cbb132937dd477e4c
Putta Khunchalee <ultimaweapon@outlook.com>
2026-05-04 17:41:29 +0700

    Adds Actions to run example as integration test

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,3 +1,32 @@
+use tokio_postgres::NoTls;
+
 mod db;
 
-fn main() {}
+fn main() {
+    let tokio = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build_local(Default::default())
+        .unwrap();
+
+    tokio.block_on(run())
+}
+
+async fn run() {
+    // Conntect to PostgreSQL.
+    let config = "host=localhost user=postgres password=postgres";
+    let (pg, con) = tokio_postgres::connect(config, NoTls).await.unwrap();
+    let con = tokio::task::spawn_local(async move { con.await.unwrap() });
+
+    // Migrate database.
+    let logger = std::io::stdout();
+    let history_table = "migrations";
+
+    porm::migration::migrate(&pg, logger, history_table, &self::db::MIGRATIONS)
+        .await
+        .unwrap();
+
+    // Shutdown.
+    drop(pg);
+
+    con.await.unwrap();
+}
